c2cb656d2117d5f1e474c6c8bd435d3296236242
EXY007 <luis70medin@gmail.com>
2024-05-09 18:57:24 -0600

    primer comit para cargar a github

---
+++ Display.js
@@ -0,0 +1,18 @@
+class Display{
+
+    constructor(displayValorAnterior,DisplayValorActual)
+    {
+        this.DisplayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculadora = new Calculadora();
+        this.ValorActual = " ";
+        this.ValorAnterior = ""; 
+
+
+
+    }
+
+    
+
+
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+const displayValorAnterior = document.getElementById("valor-anterior");
+const displayValorActual = document.getElementById("valor-actual");
+const botonesNumero = document.querySelectorAll(".numero");
+const botonesOperadores = document.querySelectorAll(".operador");
+
+const calculadora = new Calculadora();
+
+
+console.log(calculadora.sumar(4,5));
+
+
